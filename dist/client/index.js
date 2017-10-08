29a5c664913d2b8abd487e6931e757462f3b4399
Lau <balaurzor@gmail.com>
2017-10-08 14:03:11 +0200

    refactor(): Remove dist

--- dist/client/index.js
+++
@@ -1,9 +0,0 @@
-var client_app = require('express')();
-var client_http = require('http').Server(client_app);
-var path = require('path');
-var client_port = 3000;
-var connection;
-client_app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-client_http.listen(client_port, function () { return console.log('listening on port ' + client_port); });
